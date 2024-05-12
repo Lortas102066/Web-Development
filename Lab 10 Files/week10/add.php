e2a64568ecadfb177f3a3b94379985fc7a13328a
Lortas102066 <mitsukimorinaga@gmail.com>
2024-05-12 19:49:53 +1000

    New Repo

---
+++ Lab 10 Files/week10/add.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add New Page</title>
+	<link rel="stylesheet" href="admin.css" />
+</head>
+<body>
+
+    <header>
+        <h1>Add New Page</h1>
+        <nav>
+          <a href="index.php">View Site</a>
+          <a href="list.php">List Pages</a>
+          <a href="add.php">Add New Page</a>
+        </nav>
+    </header>
+
+    <main>
+       
+	   <!-- form to create new page -->
+	   
+    </main>
+
+</body>
+</html>
---
+++ Lab 10 Files/week10/save.php
@@ -0,0 +1,31 @@
+<!-- open database connection -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Save Article</title>
+	<link rel="stylesheet" href="admin.css" />
+</head>
+<body>
+
+    <header>
+        <h1>Save Article</h1>
+        <nav>
+          <a href="index.php">View Site</a>
+          <a href="list.php">List Pages</a>
+          <a href="add.php">Add New Page</a>
+        </nav>
+    </header>
+
+    <main>
+
+        <!-- save new page to database -->
+        
+    </main>
+
+</body>
+</html>
+
+<!-- close database connection -->
