52f788ff1de212d87cee7a6d57b9db415af76bbd
Lortas102066 <mitsukimorinaga@gmail.com>
2024-05-25 22:05:24 +1000

    jquery added

--- Assignment3/js/script.js
+++ Assignment3/js/script.js
@@ -5,23 +5,51 @@ $(".cs-button-solid-menu").click(function(){
 $(document).ready(function(){
     $("#tabswitcher1").click(function(event){
         event.preventDefault();
-        $("#tab-1").show();
-        $("#tab-2, #tab-3").hide();
+        $("#tab-1").show().addClass("active");
+        $(".a1").addClass("active");
+        $("#tabswitcher1").addClass("active");
+        $("#tab-2, #tab-3").hide().removeClass("active");
+        $(".a2, .a3").removeClass("active");
+        $("#tabswitcher2, #tabswitcher3").removeClass("active");
     });
 
     $("#tabswitcher2").click(function(event){
         event.preventDefault();
-        $("#tab-2").show();
-        $("#tab-1, #tab-3").hide();
+        $("#tab-2").show().addClass("active");
+        $(".a2").addClass("active");
+        $("#tabswitcher2").addClass("active");
+        $("#tab-1, #tab-3").hide().removeClass("active");
+        $(".a1, .a3").removeClass("active");
+        $("#tabswitcher1, #tabswitcher3").removeClass("active");
     });
 
     $("#tabswitcher3").click(function(event){
         event.preventDefault();
-        $("#tab-3").show();
-        $("#tab-1, #tab-2").hide();
+        $("#tab-3").show().addClass("active");     
+        $(".a3").addClass("active");
+        $("#tabswitcher3").addClass("active");
+        $("#tab-2, #tab-1").hide().removeClass("active");
+        $(".a2, .a1").removeClass("active");
+        $("#tabswitcher2, #tabswitcher1").removeClass("active");
     });
 
-    // Initially show the first tab content
-    $("#tab-1").show();
-    $("#tab-2, #tab-3").hide();
+    $("#tab-1").show().addClass("active");
+    $(".a1").addClass("active");
+    $("#tabswitcher1").addClass("active");
+    $("#tab-2, #tab-3").hide().removeClass("active");
+    $(".a2, .a3").removeClass("active");
+    $("#tabswitcher2, #tabswitcher3").removeClass("active");
+});
+
+var typed = new Typed('#element', {
+    strings: 
+    [   
+        "Welcome to Sea Bites!",
+        "Welcome to Seaside Delights, your neighborhood go-to for scrumptious fish and chips! Nestled in the heart of the community, our restaurant serves up a delicious array of golden, crisply battered fish accompanied by perfectly fried chips that promise a delightful crunch with every bite. Our cozy, maritime-themed decor provides a warm, welcoming atmosphere where family and friends can gather to enjoy hearty, flavorsome meals. At Seaside Delights, we're dedicated to bringing you the freshest seafood and a memorable dining experience with every visit. Come in and taste the tradition!"
+        
+    ],
+        startDelay: 0,
+        typeSpeed: 15,
+        backSpeed: 30,
+        loop: true,
 });
