41ca93d6a3e69dcd9793ee2c3283213725fe84f0
Lortas102066 <mitsukimorinaga@gmail.com>
2024-05-26 10:54:33 +1000

    completed web

--- Assignment3/js/script.js
+++ Assignment3/js/script.js
@@ -53,3 +53,31 @@ var typed = new Typed('#element', {
         backSpeed: 30,
         loop: true,
 });
+
+let swiperCards = new Swiper(".card__content", {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+  
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    breakpoints:{
+      600: {
+        slidesPerView: 2,
+      },
+      968: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
